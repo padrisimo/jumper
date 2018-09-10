01b62e80530fe53f9e1a523f3209a4d5019b1811
padrisimo <adrianfernandezchamizo@gmail.com>
2018-09-10 20:16:42 +0200

    parallax effect

--- Assets/Scripts/GameContoller.cs
+++ Assets/Scripts/GameContoller.cs
@@ -5,7 +5,10 @@ using UnityEngine.UI;
 
 public class GameContoller : MonoBehaviour {
 
-    
+    [Range(0f, 0.20f)]
+    public float parallaxSpeed = 0.02f;
+    public RawImage background;
+    public RawImage platform;
 
 
 	// Use this for initialization
@@ -15,6 +18,9 @@ public class GameContoller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        float finalSpeed = parallaxSpeed * Time.deltaTime;
+        background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+        platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
+
+    }
 }
