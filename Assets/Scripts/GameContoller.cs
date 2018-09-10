293654271266faf7915af8c6c82c0ae01eecfb39
padrisimo <adrianfernandezchamizo@gmail.com>
2018-09-10 20:35:24 +0200

    start game on click

--- Assets/Scripts/GameContoller.cs
+++ Assets/Scripts/GameContoller.cs
@@ -3,24 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameContoller : MonoBehaviour {
+public class GameContoller : MonoBehaviour
+{
 
     [Range(0f, 0.20f)]
     public float parallaxSpeed = 0.02f;
     public RawImage background;
     public RawImage platform;
 
+    public enum GameState { Idle, Playing };
+    public GameState gameState = GameState.Idle;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        float finalSpeed = parallaxSpeed * Time.deltaTime;
-        background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
-        platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Game starts
+        if (gameState == GameState.Idle && (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0)))
+        {
+            gameState = GameState.Playing;
+        }
+
+        //Game Runing
+        else if (gameState == GameState.Playing)
+        {
+
+            float finalSpeed = parallaxSpeed * Time.deltaTime;
+            background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+            platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
+        }
 
     }
 }
