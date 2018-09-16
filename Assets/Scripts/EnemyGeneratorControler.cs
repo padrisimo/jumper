4f313eecf8559d81091401c9199b40de5d710b7b
padrisimo <adrianfernandezchamizo@gmail.com>
2018-09-16 18:42:04 +0200

    enemies generator engine

--- Assets/Scripts/EnemyGeneratorControler.cs
+++ Assets/Scripts/EnemyGeneratorControler.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyGeneratorControler : MonoBehaviour {
+public class EnemyGeneratorControler : MonoBehaviour
+{
 
-	public GameObject enemyPrefab;
+    public GameObject enemyPrefab;
+    public float generatorTimer = 1.75f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void CreateEnemy()
+    {
+        Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+    }
+
+    public void StartGenerator()
+    {
+        InvokeRepeating("CreateEnemy", 0f, generatorTimer);
+
+    }
+
+    public void CancelGenerator()
+    {
+        CancelInvoke("CreateEnemy");
+    }
 }
--- Assets/Scripts/GameContoller.cs
+++ Assets/Scripts/GameContoller.cs
@@ -1,53 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class GameContoller : MonoBehaviour
-{
-
-    [Range(0f, 0.20f)]
-    public float parallaxSpeed = 0.02f;
-    public RawImage background;
-    public RawImage platform;
-    public GameObject uiIdle;
-
-    public enum GameState { Idle, Playing };
-    public GameState gameState = GameState.Idle;
-
-    public GameObject player;
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        // Game starts
-        if (gameState == GameState.Idle && (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0)))
-        {
-            gameState = GameState.Playing;
-            uiIdle.SetActive(false);
-            player.SendMessage("UpdateState", "PlayerRun");
-        }
-
-        //Game Runing
-        else if (gameState == GameState.Playing)
-        {
-            Parallax();
-        }
-
-    }
-
-    void Parallax()
-    {
-        float finalSpeed = parallaxSpeed * Time.deltaTime;
-        background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
-        platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
-    }
-
-}
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;using UnityEngine.UI;public class GameContoller : MonoBehaviour{    [Range(0f, 0.20f)]    public float parallaxSpeed = 0.02f;    public RawImage background;    public RawImage platform;    public GameObject uiIdle;    public enum GameState { Idle, Playing };    public GameState gameState = GameState.Idle;    public GameObject player;    public GameObject enemyGenerator;    // Use this for initialization    void Start()    {    }    // Update is called once per frame    void Update()    {        // Game starts        if (gameState == GameState.Idle && (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0)))        {            gameState = GameState.Playing;            uiIdle.SetActive(false);            player.SendMessage("UpdateState", "PlayerRun");            enemyGenerator.SendMessage("StartGenerator");        }        //Game Runing        else if (gameState == GameState.Playing)        {            Parallax();        }    }    void Parallax()    {        float finalSpeed = parallaxSpeed * Time.deltaTime;        background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);        platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);    }}
